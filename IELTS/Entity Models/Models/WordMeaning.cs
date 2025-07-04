3403fe2302e8dce43ba78cb7845510e00a727806
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-07-04 23:50:54 +0530

    EF & DbContext Setup for project.
    Enhance data handling with Entity Framework Core
    
    - Updated `Dashboard.razor` for button consistency.
    - Added Entity Framework Core packages in `IELTS.csproj`.
    - Updated connection strings in `appsettings.Development.json` and `appsettings.json`.
    - Introduced `IELTSStoreDbContext` for database context management.
    - Added `WordMeaningConfig` for entity configuration.
    - Defined `WordMeaning` model with relevant properties.
    - Created migration files for `WordMeanings` table and model snapshot.

---
+++ IELTS/Entity Models/Models/WordMeaning.cs
@@ -0,0 +1,20 @@
+﻿namespace IELTS.Entity_Models.Models
+{
+    public class WordMeaning
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string PartofSpeech { get; set; }
+        public string Pronunciation { get; set; }
+        public string Definition { get; set; }
+        public string Usage { get; set; }
+        public string RelatedWords { get; set; }
+        public string MoreInfo { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+        public string DeletedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public DateTime? DeletedOn { get; set; }
+    }
+}

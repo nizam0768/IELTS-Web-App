24cf4ea3a31ce714246b29c1f327fd05b46659e2
Mohamad Nizam Ali <52019648+nizam0768@users.noreply.github.com>
2025-07-20 21:49:28 +0530

    Feature/mark my favorite words (#23)
    * Add favorite words feature with checkbox support
    
    This commit introduces a new feature for managing favorite words in the application. A checkbox is added in the `Vocabulary.razor` file, allowing users to mark a word as a favorite. The `OnFavoriteChanged` method is triggered on checkbox state change, which calls `AddToMyFavorite` to save the favorite word to the database.
    
    The `MyFavoriteWords` entity is created to represent favorite words, and the `IELTSStoreDbContext` is updated to include a `DbSet<MyFavoriteWords>`. Corresponding migration files are added to define the schema for the `MyFavoriteWords` table. Additionally, CSS styles are included to enhance the appearance of the checkbox.
    
    * Enhance favorite word management in Vocabulary.razor
    
    - Added dependency injections for IJSRuntime and IDbContextFactory.
    - Introduced OnFavoriteChanged method to handle favorite status changes.
    - Modified MyFavoriteWord to manage adding/removing favorites.
    - Removed AddToMyFavorite; its logic is now in MyFavoriteWord.
    - Added SaveAndNotifyAsync for saving changes and notifying users.
    - Implemented ShowPopUpAsync to display alerts for user feedback.
    
    * Enhance favorite word feature in vocabulary app
    
    - Introduced `isFavoriteProcessing` to manage favorite updates.
    - Added backing field `_isFavorite` with a setter for `isFavorite`.
    - Updated `NextWord` and `PrevWord` methods to check favorite status asynchronously.
    - Modified `OnInitializedAsync` to handle empty word lists and initialize `_isFavorite`.
    - Removed `OnFavoriteChanged` and integrated its logic into `UpdateFavorite`.
    - Replaced `SaveAndNotifyAsync` with direct save and notification calls in `MyFavoriteWord`.
    
    * Placing checkbox in flash card to mark user favorite word.

---
+++ IELTS/EntityModels/Migrations/20250715171900_MyFavoriteWordTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IELTS.EntityModels.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyFavoriteWordTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MyFavoriteWords",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WordId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyFavoriteWords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MyFavoriteWords_WordMeanings_WordId",
+                        column: x => x.WordId,
+                        principalSchema: "dbo",
+                        principalTable: "WordMeanings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MyFavoriteWords_WordId",
+                schema: "dbo",
+                table: "MyFavoriteWords",
+                column: "WordId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MyFavoriteWords",
+                schema: "dbo");
+        }
+    }
+}

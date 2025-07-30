a73a224eaf5dee3b400e0fa237659f3d3e43ed24
Muhammed Nizam Ali <nizamali985@gmail.com>
2025-07-30 23:58:42 +0530

    toster fix.

--- IELTS/wwwroot/lib/site.js
+++ IELTS/wwwroot/lib/site.js
@@ -1,9 +1,5 @@
 ﻿window.blazorInterop = window.blazorInterop || {};
 
-window.blazorInterop.showAlert = function (title, message, type, duration) {
-    $.toast({ title, message, type, duration });
-};
-
 window.blazorInterop.applyDataTable = function (tableId) {
     setTimeout(() => {
         const table = document.getElementById(tableId);
@@ -24,30 +20,3 @@ window.blazorInterop.applyDataTable = function (tableId) {
         }
     });
 };
-
-//function InitTable() {
-//    $("#example").DataTable();
-//}
-//window.applyDataTable = function (tableId) {
-//    setTimeout(function () {
-//        const table = document.getElementById(tableId);
-//        if (table) {
-//            console.log("Table found. Trying to initialize DataTable.");
-//            $('#' + tableId).DataTable({
-//                order: [[0, 'asc']],
-//                scrollX: true,
-//                paging: true,
-//                info: true,
-//                columnDefs: [
-//                    { width: '15%', targets: 0 },  
-//                    { width: '10%', targets: 1 }, 
-//                    { width: '15%', targets: 2 }, 
-//                    { width: '40%', targets: 3 },
-//                    { width: '20%', targets: 4 } 
-//                ]
-//            });
-//        } else {
-//            console.warn('Table not found:', tableId);
-//        }
-//    });
-//};
